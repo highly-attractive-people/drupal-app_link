eebf2c36428fadfae27769b8ca2be90ee8c5be03
Rob Colburn <rob@robcolburn.com>
2015-07-21 15:58:12 -0700

    DRY up test scripts

--- test/app_link.js
+++ test/app_link.js
@@ -10,13 +10,16 @@ var PLATFORMS = require("./fixtures/example-platforms.json");
 require("./fixtures/http-server").listen(3000);
 var BASE_URL = "http://localhost:3000/";
 
-var UNSUPPORTED_DESKTOP = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36";
-var IPHONE = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25";
-var IPAD = "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25";
-var ANDROID = "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
-var ANDROID_CHROME = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
-var WINDOWS_PHONE = "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)";
-var KINDLE = "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Kindle Fire Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+var USERAGENTS = {
+  "Unsupported": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+  "iPhone": "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25",
+  "iPod": "Mozilla/5.0 (iPod touch; CPU iPhone OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53",
+  "iPad": "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25",
+  "Android Browser": "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+  "Android Chrome": "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19",
+  "Windows Phone": "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
+  "Kindle Fire": "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Kindle Fire Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+};
 
 describe("Test Applink Redirects", function() {
   // This spawns a separate PhantomJS process that will talk back asyncronously via WebSocket.
@@ -31,11 +34,15 @@ describe("Test Applink Redirects", function() {
     phantom_instance.exit();
   });
 
+  /** @var {object} applink: Data about this applink. Keyed by Platform ID */
+  var applink;
+  beforeEach(function() {
+    applink = clone(PLATFORMS);
+  });
+
   /**
    * Test a given app link
    *   - We will take the useragent and lie to Phantom.
-   * @param {object} applink
-   *   Data about this applink. Keyed by Platform ID
    * @param {object} options
    *   - {string} useragent
    *     User Agent that Phantom should prentend to be.
@@ -50,8 +57,8 @@ describe("Test Applink Redirects", function() {
    * @param {function} done.
    *   Callback to call when done, called with Error if failure.
    */
-  function testApplinkRedirect (applink, options, targetURL, done) {
-    var useragent = options.useragent || UNSUPPORTED_DESKTOP;
+  function testApplinkRedirect (options, targetURL, done) {
+    var useragent = options.useragent || USERAGENTS.Unsupported;
     var baseURL = options.baseURL || BASE_URL;
     phantom_instance.createPage(function (page) {
       // The "onNavigationRequested" event is async and can easily fire in an
@@ -135,70 +142,91 @@ describe("Test Applink Redirects", function() {
         });
     });
   }
- 
-  /**
-   * Base line testing for all of our platforms
-   */
-  describe("Platform routing", function() {
-    var applink;
-    beforeEach(function() {
-      applink = clone(PLATFORMS);
-    });
-    it("Unsupported Desktop should go to the Fallback.", function(done) {
-      testApplinkRedirect(applink, {}, applink.app_link_platform_fallback.fallback_url, done);
-    });
-    it("iPhone should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: IPHONE}, applink.app_link_platform_iphone.app_url, done);
-    });
-    it("iPhone should go to the App Store Url w/o app_url.", function(done) {
-      applink.app_link_platform_iphone.app_url = "";
-      testApplinkRedirect(applink, {useragent: IPHONE}, applink.app_link_platform_iphone.store_url, done);
-    });
-    it("iPad should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: IPAD}, applink.app_link_platform_ipad.app_url, done);
-    });
-    it("iPad should go to the App Store Url w/o app_url.", function(done) {
-      applink.app_link_platform_ipad.app_url = "";
-      testApplinkRedirect(applink, {useragent: IPAD}, applink.app_link_platform_ipad.store_url, done);
-    });
-    it("Android Browser should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: ANDROID}, applink.app_link_platform_android.app_url, done);
-    });
-    it("Android Browser should go to the App Store Url w/o app_url.", function(done) {
-      applink.app_link_platform_android.app_url = "";
-      testApplinkRedirect(applink, {useragent: ANDROID}, applink.app_link_platform_android.store_url, done);
-    });
-    it("Android Chrome should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME}, applink.app_link_platform_android.intent_url, done);
-    });
-    it("Android Chrome should go to the App Store Url w/o intent_url.", function(done) {
-      applink.app_link_platform_android.intent_url = "";
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME}, applink.app_link_platform_android.store_url, done);
-    });
-    it("Windows Phone should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: WINDOWS_PHONE}, applink.app_link_platform_windows_phone.app_url, done);
-    });
-    it("Windows Phone should go to the App Store Url w/o app_url.", function(done) {
-      applink.app_link_platform_windows_phone.app_url = "";
-      testApplinkRedirect(applink, {useragent: WINDOWS_PHONE}, applink.app_link_platform_windows_phone.store_url, done);
-    });
-    it("Kindle Fire should go to the App Url.", function(done) {
-      testApplinkRedirect(applink, {useragent: KINDLE}, applink.app_link_platform_kindle_fire.app_url, done);
-    });
-    it("Kindle Fire should go to the App Store Url w/o app_url.", function(done) {
-      applink.app_link_platform_kindle_fire.app_url = "";
-      testApplinkRedirect(applink, {useragent: KINDLE}, applink.app_link_platform_kindle_fire.store_url, done);
-    });
-  });
 
+  describe("Platform", function() {
+    it("Unsupported Desktop to the Fallback.", function(done) {
+      testApplinkRedirect({}, applink.app_link_platform_fallback.fallback_url, done);
+    });
+    function testPlatform (browser, key) {
+      key = "app_link_platform_" + key;
+      it(browser + " to the App Url.", function(done) {
+        var target = browser === "Android Chrome" ? "intent_url" : "app_url";
+        testApplinkRedirect({useragent: USERAGENTS[browser]}, applink[key][target], done);
+      });
+      it(browser + " to the App Store Url w/o app_url.", function(done) {
+        applink[key].app_url = applink[key].intent_url = "";
+        testApplinkRedirect({useragent: USERAGENTS[browser]}, applink[key].store_url, done);
+      });
+      it(browser + " should go to the App Store Url w/o app_url or store_url.", function(done) {
+        applink[key].app_url = applink[key].intent_url = applink[key].store_url = "";
+        testApplinkRedirect({useragent: USERAGENTS[browser]}, applink.app_link_platform_fallback.fallback_url, done);
+      });
+    }
+    testPlatform("iPhone", "iphone");
+    testPlatform("iPod", "iphone");
+    testPlatform("iPad", "ipad");
+    testPlatform("Android Browser", "android");
+    testPlatform("Android Chrome", "android");
+    testPlatform("Windows Phone", "windows_phone");
+    testPlatform("Kindle Fire", "kindle_fire");
+   });
+ 
   /**
    * Limit scope to distinct cases: URI Schemes (most) & Intent URLs (Android)
    */
   describe("URL Processing on URI Schemes", function() {
-    var applink;
-    beforeEach(function() {
-      applink = clone(PLATFORMS);
-    });
+    function testPlatform (browser, key, type) {
+      key = "app_link_platform_" + key;
+      it(type + " should pass the querystring if supported.", function(done) {
+        applink[key].supports_qs = 1;
+        var target = applink[key][type].split("#");
+        target = target[0] +
+          (target[0].match(/\?/) ? "&" : "?") + "a=b&c=d" +
+          (target.length > 1 ? "#" + target[1] : "");
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: BASE_URL + "?a=b&c=d"},
+          target, done);
+      });
+      it(type + " should not pass the querystring if not supported.", function(done) {
+        applink[key].supports_qs = 0;
+        var target = applink[key][type];
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: BASE_URL + "?a=b&c=d"},
+          target, done);
+      });
+      it(type + " should pass the path if supported.", function(done) {
+        applink[key].supports_path = 1;
+        var target = applink[key][type].split("#");
+        target = target[0] +
+          (target[0][target[0].length - 1] === "/" ? "" : "/") + "a/b/" +
+          (target.length > 1 ? "#" + target[1] : "");
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: BASE_URL + "?path=/a/b/"},
+          target, done);
+      });
+      it(type + " should not pass the path if not supported.", function(done) {
+        applink[key].supports_path = 0;
+        var target = applink[key][type];
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: BASE_URL + "?path=/a/b/"},
+          target, done);
+      });
+      it(type + " should pass the querystring to Store if supported.", function(done) {
+        applink[key][type] = "";
+        applink[key].supports_store_qs = 1;
+        var base = BASE_URL +
+          (BASE_URL.match(/\?/) ? "&" : "?") + "a=b&c=d";
+        var target = applink[key].store_url +
+          (applink[key].store_url.match(/\?/) ? "&" : "?") + "a=b&c=d";
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: base},
+          target, done);
+      });
+      it(type + " should not pass the querystring to Store if not supported.", function(done) {
+        applink[key][type] = "";
+        applink[key].supports_store_qs = 0;
+        var target = applink[key].store_url;
+        testApplinkRedirect({useragent: USERAGENTS[browser], baseURL: BASE_URL + "?a=b&c=d"},
+          target, done);
+      });
+    }
+    testPlatform("iPhone", "iphone", "app_url");
+    testPlatform("Android Chrome", "android", "intent_url");
     // TODO: Fallback is processed is server-side logic.  Not currently Testable client-side.
     // This is tough to port since we need it in HTML places like <link rel=canonical />
     // it("Fallback should pass the querystring if supported.", function(done) {
@@ -208,96 +236,6 @@ describe("Test Applink Redirects", function() {
     //     done
     //   );
     // });
-    it("URI Schemes should pass the querystring if supported.", function(done) {
-      applink.app_link_platform_iphone.supports_qs = 1;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_iphone.app_url + "?a=b&c=d",
-        done
-      );
-    });
-    it("URI Schemes should not pass the querystring if not supported.", function(done) {
-      applink.app_link_platform_iphone.supports_qs = 0;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_iphone.app_url,
-        done
-      );
-    });
-    it("URI Schemes should pass the path if supported.", function(done) {
-      applink.app_link_platform_iphone.supports_path = 1;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?path=/a/b/"},
-        applink.app_link_platform_iphone.app_url + "a/b/",
-        done
-      );
-    });
-    it("URI Schemes should not pass the path if not supported.", function(done) {
-      applink.app_link_platform_iphone.supports_path = 0;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?path=/a/b/"},
-        applink.app_link_platform_iphone.app_url,
-        done
-      );
-    });
-    it("URI Schemes should pass the querystring to Store if supported.", function(done) {
-      applink.app_link_platform_iphone.app_url = "";
-      applink.app_link_platform_iphone.supports_store_qs = 1;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_iphone.store_url + "&a=b&c=d",
-        done
-      );
-    });
-    it("URI Schemes should not pass the querystring to Store if not supported.", function(done) {
-      applink.app_link_platform_iphone.app_url = "";
-      applink.app_link_platform_iphone.supports_store_qs = 0;
-      testApplinkRedirect(applink, {useragent: IPHONE, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_iphone.store_url,
-        done
-      );
-    });
-    it("Intent URLs should pass the querystring if supported.", function(done) {
-      applink.app_link_platform_android.supports_qs = 1;
-      var targetParts = applink.app_link_platform_android.intent_url.split("#");
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?a=b&c=d"},
-        (targetParts[0] + "?a=b&c=d#" + targetParts[1]),
-        done
-      );
-    });
-    it("Intent URLs should not pass the querystring if not supported.", function(done) {
-      applink.app_link_platform_android.supports_qs = 0;
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_android.intent_url,
-        done
-      );
-    });
-    it("Intent URLs should pass the path if supported.", function(done) {
-      applink.app_link_platform_android.supports_path = 1;
-      var targetParts = applink.app_link_platform_android.intent_url.split("#");
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?path=/a/b/"},
-        (targetParts[0] + "/a/b/#" + targetParts[1]),
-        done
-      );
-    });
-    it("Intent URLs should not pass the path if not supported.", function(done) {
-      applink.app_link_platform_android.supports_path = 0;
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?path=/a/b/"},
-        applink.app_link_platform_android.intent_url,
-        done
-      );
-    });
-    it("Intent URLs should pass the querystring to Store if supported.", function(done) {
-      applink.app_link_platform_android.intent_url = "";
-      applink.app_link_platform_android.supports_store_qs = 1;
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_android.store_url + "&a=b&c=d",
-        done
-      );
-    });
-    it("Intent URLs should not pass the querystring to Store if not supported.", function(done) {
-      applink.app_link_platform_android.intent_url = "";
-      applink.app_link_platform_android.supports_store_qs = 0;
-      testApplinkRedirect(applink, {useragent: ANDROID_CHROME, baseURL: BASE_URL + "?a=b&c=d"},
-        applink.app_link_platform_android.store_url,
-        done
-      );
-    });
   });
 
   /**
